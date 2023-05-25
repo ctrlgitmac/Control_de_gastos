e12d291e9d31d500235d665ee71c9edcc4277db8
ctrlgitmac <rom.vfy@gmail.com>
2023-05-25 08:10:47 -0500

    iniciando

--- control-g/vite.config.js
+++ control-g/vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/control-g',
+  base: 'Control_de_gastos/control-g',
   plugins: [vue()],
   resolve: {
     alias: {
